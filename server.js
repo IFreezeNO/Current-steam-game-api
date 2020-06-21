9e5f19eb92e727fdcadda379700b5f75689ba380
IFreezeNO <46674863+IFreezeNO@users.noreply.github.com>
2020-06-21 23:20:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/faceit/:faceitname', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('X-XSS-Protection' , 1 );
-
+var faceitelo = undefined;
 var faceiturl = `https://api.faceit.com/core/v1/nicknames/${req.params.faceitname}`
 
 request({
@@ -27,7 +27,7 @@ request({
 }, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
-       if(body.payload.games.csgo.faceit_elo === undefined) {
+       if(faceitelo === undefined) {
         return res.status(400).json({ error : 'Could not find the user'});
         }
         var faceitelo = body.payload.games.csgo.faceit_elo;
