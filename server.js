754142837522d96ee0b973bf6f1c963450fc82c7
IFreezeNO <46674863+IFreezeNO@users.noreply.github.com>
2020-06-21 23:48:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ request({
         
        if(body.result == "error") {
         return res.status(400).json({ error : 'Could not find the user'});
-        } else if(games[0] === "csgo") {
+        } else if(body.payload.games[0] === "csgo") {
         var faceitelo = body.payload.games.csgo.faceit_elo;
         }else {
              res.status(400).json({ error : 'Could not find the user, remember this is case sensitive'});
